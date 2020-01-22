a9de09f686b02a61d017c852ec11493770d14134
johnkirtley <kirtleyj16@gmail.com>
2020-01-22 15:43:49 -0800

    Changed image

--- src/components/CardContainer/ImageList.js
+++ src/components/CardContainer/ImageList.js
@@ -5,9 +5,11 @@ import axios from "axios";
 export default function ImageList() {
     const [images, setImages] = useState([]);
 
+
+
     useEffect(() => {
         axios
-            .get("https://api.nasa.gov/planetary/apod?api_key=DmCR5nVfoXyDYmhE5zgdZaUMwJkcmvt64q5Pte48")
+            .get("https://api.nasa.gov/planetary/apod?api_key=DmCR5nVfoXyDYmhE5zgdZaUMwJkcmvt64q5Pte48&date=2020-1-5")
             .then(res => {
                 setImages(res.data);
             })
